ada7c359db44d837ccd6bdea03c5d3062b26287b
tntra-tusharbhoraniya <tushar.bhoraniya@tntra.io>
2026-09-21 08:27:51 +0000

    Test Brakeman security failure

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,7 @@ class TasksController < ApplicationController
 
   # GET /tasks or /tasks.json
   def index
-    @tasks = Task.all
+  @tasks = Task.where("title = '#{params[:title]}'")
   end
 
   # GET /tasks/1 or /tasks/1.json
